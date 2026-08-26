3e5c3e486994d70ae97a734668acb411987c007f
Er.TanmayGupta <tanmay.gupta2214@gmail.com>
2026-08-27 00:02:10 +0530

    [Tanmay] Add the solution of this problem

---
+++ Q29xolution.java
@@ -0,0 +1,20 @@
+// LeetCode problem no.29: Divide Two Integer
+import java.util.*;
+
+public class Q29xolution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int divident = sc.nextInt();
+        int divisor = sc.nextInt();
+        int ans = divide(divident , divisor);
+
+        System.out.println(ans);
+    }
+
+    public static int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        return (dividend / divisor);
+    }
+}
