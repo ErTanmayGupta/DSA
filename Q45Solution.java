ec787db0b52f03c25577f94e007036e5d7b26031
Er.TanmayGupta <tanmay.gupta2214@gmail.com>
2026-09-08 20:46:23 +0530

    [Tanmay] add the solution of Leetcode problem No.45 - Jump Game II

---
+++ Q45Solution.java
@@ -0,0 +1,29 @@
+// Leetcode Problem No. 45: Jump Game II
+import java.util.*;
+class Q45Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(jump(nums));
+    }
+
+    public static int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
