a77f9975cc97926b382569cfff0f5b187fb03d9a
Er.TanmayGupta <tanmay.gupta2214@gmail.com>
2026-09-19 22:55:56 +0530

    [Tanmay] add the solution of Leetcode problem No. 69 Sqrt(x)

---
+++ 1-1000/Q69Solution.java
@@ -0,0 +1,33 @@
+// LeetCode Problem No.69 Sqrt(x)
+import java.util.*;
+class Q69Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(mySqrt(x));
+    }
+
+    public static int mySqrt(int x) {
+        if(x < 2){
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int ans = 0;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(mid <= x / mid){
+                ans = mid;
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
