7d1d41f40cb02bc2d118092c7186488515fcef43
Er.TanmayGupta <tanmay.gupta2214@gmail.com>
2026-09-20 20:40:35 +0530

    [Tanmay] add the solution of Leetcode problem No. 70 : Climbing Stairs

---
+++ 1-1000/Q70Solution.java
@@ -0,0 +1,26 @@
+// LeetCode Problem 70: Climbing Stairs
+import java.util.*;
+public class Q70Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = climbStairs(n);
+        System.out.println("Number of distinct ways to climb to the top: " + result);
+        
+    }
+
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1; 
+        dp[2] = 2; 
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; 
+        }
+
+        return dp[n];
+    }
+}
